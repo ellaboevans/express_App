5eefda88044374b36fef3962bcf5fbbc660cb029
Evans <ellaboevans@gmail.com>
2023-02-11 10:34:00 +0000

    Modify file path

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require("express");
 
 const app = express();
 
-app.get("/index.html", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile("./pages/index.html", { root: __dirname });
 });
 
-app.get("/about.html", (req, res) => {
+app.get("/about", (req, res) => {
   res.sendFile("./pages/about.html", { root: __dirname });
 });
 
-app.get("/portfolio.html", (req, res) => {
+app.get("/portfolio", (req, res) => {
   res.sendFile("./pages/portfolio.html", { root: __dirname });
 });
 
