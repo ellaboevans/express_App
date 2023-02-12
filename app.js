6439248cf89224a7d2a3fb7a5ebb7ad694de7214
Evans <ellaboevans@gmail.com>
2023-02-12 22:16:18 +0000

    Inject Properties
    Also added an array of object that renders data to the browser dynamically

--- app.js
+++ app.js
@@ -1,27 +1,74 @@
 const express = require("express");
 
+// Express App
 const app = express();
 
+// Register view engine
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.status(200).sendFile("./pages/index.html", { root: __dirname });
+  const blogs = [
+    { title: "Evans Elabo", snippet: "Lorem ipsum dolor sit amet consectetur" },
+    {
+      title: "Concept Creator",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Code Concept",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.status().sendFile("./pages/about.html", { root: __dirname });
+  const paragraphs = [
+    {
+      title: "",
+      snippet:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur fugit officia minus quibusdam nobis excepturi maiores neque molestiae autem corrupti. Cumque dicta, sapiente molestiae repudiandae mollitia libero at dignissimos inventore necessitatibus obcaecati velit quo neque similique eos magnam culpa doloremque numquam voluptas aut? Reiciendis expedita corporis labore nulla tempora. Recusandae tempora ",
+    },
+    {
+      title: "",
+      snippet:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur fugit officia minus quibusdam nobis excepturi maiores neque molestiae autem corrupti. Cumque dicta, sapiente molestiae repudiandae mollitia libero at dignissimos inventore necessitatibus obcaecati velit quo neque similique eos magnam culpa doloremque numquam voluptas aut? Reiciendis expedita corporis labore nulla tempora. Recusandae tempora ",
+    },
+    {
+      title: "",
+      snippet:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur fugit officia minus quibusdam nobis excepturi maiores neque molestiae autem corrupti. Cumque dicta, sapiente molestiae repudiandae mollitia libero at dignissimos inventore necessitatibus obcaecati velit quo neque similique eos magnam culpa doloremque numquam voluptas aut? Reiciendis expedita corporis labore nulla tempora. Recusandae tempora ",
+    },
+    {
+      title: "",
+      snippet:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur fugit officia minus quibusdam nobis excepturi maiores neque molestiae autem corrupti. Cumque dicta, sapiente molestiae repudiandae mollitia libero at dignissimos inventore necessitatibus obcaecati velit quo neque similique eos magnam culpa doloremque numquam voluptas aut? Reiciendis expedita corporis labore nulla tempora. Recusandae tempora ",
+    },
+    {
+      title: "",
+      snippet:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur fugit officia minus quibusdam nobis excepturi maiores neque molestiae autem corrupti. Cumque dicta, sapiente molestiae repudiandae mollitia libero at dignissimos inventore necessitatibus obcaecati velit quo neque similique eos magnam culpa doloremque numquam voluptas aut? Reiciendis expedita corporis labore nulla tempora. Recusandae tempora ",
+    },
+    {
+      title: "",
+      snippet:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur fugit officia minus quibusdam nobis excepturi maiores neque molestiae autem corrupti. Cumque dicta, sapiente molestiae repudiandae mollitia libero at dignissimos inventore necessitatibus obcaecati velit quo neque similique eos magnam culpa doloremque numquam voluptas aut? Reiciendis expedita corporis labore nulla tempora. Recusandae tempora ",
+    },
+    {
+      title: "",
+      snippet:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur fugit officia minus quibusdam nobis excepturi maiores neque molestiae autem corrupti. Cumque dicta, sapiente molestiae repudiandae mollitia libero at dignissimos inventore necessitatibus obcaecati velit quo neque similique eos magnam culpa doloremque numquam voluptas aut? Reiciendis expedita corporis labore nulla tempora. Recusandae tempora ",
+    },
+  ];
+  res.render("about", { title: "About", paragraphs });
 });
 
-app.get("/portfolio", (req, res) => {
-  res.status(200).sendFile("./pages/portfolio.html", { root: __dirname });
-});
-
-// Redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create New Blog" });
 });
 
 // 404 Page
 app.use((req, res) => {
-  res.status(404).sendFile("./pages/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
 
 app.listen(3000, "localhost", () => {
