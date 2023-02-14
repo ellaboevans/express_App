778d536c26ab7f1e37400a4c8a7ef0959f05e3fb
Evans <ellaboevans@gmail.com>
2023-02-14 21:33:22 +0000

    Import Morgan Middleware

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const morgan = require("morgan");
+// console.log(morgan);
 
 // Express App
 const app = express();
@@ -10,18 +12,7 @@ app.listen(3000, "localhost", () => {
   console.log("Server successfully running on port 3000");
 });
 
-app.use((req, res, next) => {
-  console.log("new request made:");
-  console.log("host:", req.hostname);
-  console.log("path:", req.path);
-  console.log("method:", req.method);
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("in the next middleware");
-  next();
-});
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const blogs = [
