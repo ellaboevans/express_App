c23d91eb4cb40541caf09d9767d1cbc19f95bc32
Evans <ellaboevans@gmail.com>
2023-02-16 21:49:21 +0000

    Add Event to DELETE Button

--- public/main.js
+++ public/main.js
@@ -1,3 +1,19 @@
 const year = document.querySelector("#year");
 let date = new Date();
 year.textContent = date.getFullYear();
+
+// Delete Blog
+const trashcan = document.querySelector("a.delete");
+
+trashcan.addEventListener("click", (e) => {
+  const endpoints = `/blogs/${trashcan.dataset.doc}`;
+
+  fetch(endpoints, {
+    method: "DELETE",
+  })
+    .then((response) => response.json())
+    .then((data) => (window.location.href = data.redirect))
+    .catch((err) => {
+      console.log(err);
+    });
+});
