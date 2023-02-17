c68f6d7e4fcb894b173d9daa0b172455f8ba3afc
Evans <ellaboevans@gmail.com>
2023-02-17 19:12:14 +0000

    Change StrictQuery value

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
-mongoose.set("strictQuery", false);
+  mongoose.set("strictQuery", true);
 
 // Register view engine
 app.set("view engine", "ejs");
