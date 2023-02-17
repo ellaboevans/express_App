ffa4a69d73884b745f451506162617105dd95cd3
Evans <ellaboevans@gmail.com>
2023-02-17 21:04:00 +0000

    Add blogs dir to routes path

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -6,7 +6,7 @@ const blog_index = (req, res) => {
   Blog.find()
     .sort({ createdAt: -1 })
     .then((result) => {
-      res.render("index", { title: "All Blogs", blogs: result });
+      res.render("blogs/index", { title: "All Blogs", blogs: result });
     })
     .catch((err) => {
       console.log(err);
@@ -17,7 +17,7 @@ const blog_details = (req, res) => {
   const id = req.params.id;
   Blog.findById(id)
     .then((result) => {
-      res.render("details", { blog: result, title: "Blog Details" });
+      res.render("blogs/details", { blog: result, title: "Blog Details" });
     })
     .catch((err) => {
       console.log(err);
@@ -25,7 +25,7 @@ const blog_details = (req, res) => {
 };
 
 const blog_create_get = (req, res) => {
-  res.render("create", { title: "Create New Blog" });
+  res.render("blogs/create", { title: "Create New Blog" });
 };
 
 const blog_create_post = (req, res) => {
