bb434eb2bf4c4423e80c57c198494cc4a3392bd2
Evans <ellaboevans@gmail.com>
2023-02-17 17:46:05 +0000

    Move blogRoutes into Routes

---
+++ routes/blogRoutes.js
@@ -0,0 +1,57 @@
+const express = require("express");
+
+const Blog = require("../models/blog");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      conosle.log(err);
+    });
+});
+
+router.get("/create", (req, res) => {
+  res.render("create", { title: "Create New Blog" });
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result, title: "Blog Details" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/blogs" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
