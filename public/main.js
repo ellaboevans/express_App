358c61c9cba576293631875fbe6fb5b06779654b
Evans <ellaboevans@gmail.com>
2023-02-18 14:00:40 +0000

    Link Script to global pages

--- public/main.js
+++ public/main.js
@@ -1,23 +1,31 @@
 // Copyright Logic
 const year = document.querySelector("#year");
 // console.log(year);
-let date = new Date();
-year.textContent = date.getFullYear();
+const getFullYears = () => {
+  let date = new Date();
+  year.textContent = date.getFullYear();
+};
+
+getFullYears();
 
 // Delete Method on Frontend
 const trashcan = document.querySelector("a.delete");
 
-trashcan.addEventListener("click", (e) => {
-  const endpoints = `/blogs/${trashcan.dataset.doc}`;
+const deleteOnFrontend = () => {
+  trashcan.addEventListener("click", (e) => {
+    const endpoints = `/blogs/${trashcan.dataset.doc}`;
 
-  fetch(endpoints, {
-    method: "DELETE",
-  })
-    .then((response) => response.json())
-    .then((data) => (window.location.href = data.redirect))
-    .catch((err) => {
-      console.log(err);
-    });
-});
+    fetch(endpoints, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((data) => (window.location.href = data.redirect))
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
+deleteOnFrontend();
 
 
+alert("Connected to all pages!");
